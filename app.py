a58157bf8957b5ecb27f1c7b64ef66de1c64843a
MD siFaT isLaM <125746506+Cyber-Programer@users.noreply.github.com>
2024-01-05 12:24:16 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify
+import json
+
+app = Flask(__name__)
+
+# Load data from the JSON file
+with open('full_quran.json', 'r', encoding='utf-8') as file:
+    surahs_data = json.load(file)
+
+suras = surahs_data  # No need for the loop to create `suras`
+
+@app.route('/')
+def home():
+    return 'Use /surah/surah number <br>/surah/1'
+
+@app.route('/surah/<int:sura_no>', methods=['GET'])
+def get_surah_ayah(sura_no):
+    data = None
+    if sura_no > 114:
+        return 'as a muslim you need to know that there are 114 surah in quran'
+    else:
+        for sr in suras:
+            if sr['id'] == sura_no:
+                data = {
+                    "No": sura_no,
+                    "Name": sr['name'],
+                    "Transliteration": sr['transliteration'],
+                    "Type": sr['type'],
+                    "Total Ayat": sr['total_verses'],
+                    "Verses": sr['verses'],
+                }
+                return data
+            
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
